596be50501421497a13936606bf5f5a3be920f29
Hassnae BADDOU <samya1902@gmail.com>
2023-08-10 03:44:09 -0700

    all

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * check_cycle - check if a linked list contain a cycle
+ * @list: linked list
+ * Return: 1 on success, otherwise 0
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+
+	if (!list)
+		return (0);
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
