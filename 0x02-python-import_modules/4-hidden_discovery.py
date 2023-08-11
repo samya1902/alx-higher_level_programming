4556947857fa9680ea279ea91dbfeebb330628e1
Hassnae BADDOU <samya1902@gmail.com>
2023-08-11 03:36:16 -0700

    hidden

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """prints all names on hidden_4"""
+    import hidden_4
+    names = dir(hidden_4)
+    for i in names:
+        if i[:2] != "__":
+            print(i)
