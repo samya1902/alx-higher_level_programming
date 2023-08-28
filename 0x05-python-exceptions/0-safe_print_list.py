922973ced860b8f8e435bbd6c9609717596f82f4
Hassnae BADDOU <samya1902@gmail.com>
2023-08-28 02:45:10 -0700

    0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """prints x elements of a list.
+    Args:
+        my_list(list): list
+        x(int): nbr of elements
+    Returns:
+        nbr of elements printed
+    """
+    a = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i], end=""));
+            a += 1
+        except IndexError:
+            break
+    print("")
+    return (a)
