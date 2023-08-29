9ea8510a2f51f8ae70daa1a7c85e3fb7157fd82c
Hassnae BADDOU <samya1902@gmail.com>
2023-08-29 03:06:42 -0700

    square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 class Square:
-    def__init__(self, size):
-        self__size = size
+    def __init__(self, size):
+        self.__size = size
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,4 +1,4 @@
-#!/usr/python3
+#!/usr/bin/python3
 class Square:
     def __init__(self, size=0):
         if not isinstance(size, int):
@@ -6,3 +6,5 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+    def area(self):
+        return (self.__size * self.__size)
