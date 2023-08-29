5682a68fd3c6dca9bab3422dce0c5ace881fe870
Hassnae BADDOU <samya1902@gmail.com>
2023-08-29 12:32:59 -0700

    all

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """define a class Square"""
 
+
 class Square:
     """class Square"""
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
+"""Define a class Square"""
+
+
 class Square:
+    """Represent a square"""
+
     def __init__(self,size=0):
+        """Initialize new square
+
+        Args:
+            size (int): size of new square
+        """
         self.size = size
+
     @proprety
     def size(self):
+        """get/set current size of square"""
         return (self.__size)
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -12,5 +25,7 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
+        """Return current area of the square"""
         return (self.__size * self.__size)
