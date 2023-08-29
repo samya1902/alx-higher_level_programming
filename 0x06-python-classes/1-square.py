64156c6392b270c633e4f3df7d4fc4189eefa544
Hassnae BADDOU <samya1902@gmail.com>
2023-08-29 11:59:05 -0700

    1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""Define a class Square"""
+
 class Square:
+    """Represent a Square"""
+
     def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): size of the new square.
+        """
         self.__size = size
