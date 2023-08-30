e207c681cfc4e2206b7b68e296f1f72561892c16
Hassnae BADDOU <samya1902@gmail.com>
2023-08-30 03:17:25 -0700

    6

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """define classes"""
 
+
 class Node:
     """Class Node of singly linked list"""
 
@@ -35,6 +36,7 @@ class Node:
             if not isinstance(value, Node) and value is not None:
                 raise TypeError("next_node must be a Node object")
             self.__next_node = value
+
 
 class SinglyLinkedList:
     """Represent a singly linked list"""
